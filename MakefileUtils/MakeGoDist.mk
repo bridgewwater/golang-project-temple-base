6dc4841e06e86658efd2586e5bc3a73d968c651c
sinlov <sinlovgmppt@gmail.com>
2023-02-08 10:28:06 +0800

    feat: add diffetent platform support of windows building as tar file

--- MakefileUtils/MakeGoDist.mk
+++ MakefileUtils/MakeGoDist.mk
@@ -3,6 +3,8 @@
 # must as some include MakeDistTools.mk
 #
 # windows use must install tools
+# https://scoop.sh/#/apps?q=gow&s=0&d=1&o=true
+# scoop install gow
 # https://scoop.sh/#/apps?q=busybox&s=0&d=1&o=true
 # scoop install shasum
 #
@@ -41,8 +43,8 @@ define dist_tar_with_source
 	@echo "      ENV_INFO_DIST_MARK    : ${ENV_INFO_DIST_MARK}"
 	@echo ""
 	$(warning if cp source can change here cp tar undper $(strip ${1}))
-	cp ${ENV_ROOT_MANIFEST_PKG_JSON} $(strip ${1})
-	cp -R doc/ $(strip ${1})/doc/
+	cp '${ENV_ROOT_MANIFEST_PKG_JSON}' '$(strip ${1})'
+	cp -R 'doc/' '$(strip ${1})/doc'
 	@echo "-> cp source finish"
 
 	tar -zcvf $(strip ${2})${ENV_INFO_DIST_BIN_NAME}-$(strip ${3})-${ENV_INFO_DIST_VERSION}${ENV_INFO_DIST_MARK}.tar.gz -C $(strip ${1}) "."
@@ -61,8 +63,8 @@ define dist_tar_with_windows_source
 	@echo "      ENV_INFO_DIST_MARK    : ${ENV_INFO_DIST_MARK}"
 	@echo ""
 	$(warning if cp source can change here cp tar undper $(strip ${1}))
-	cp ${ENV_ROOT_MANIFEST_PKG_JSON} $(strip ${1})
-	cp -R doc\ $(strip ${1})\doc\
+	cp '${ENV_ROOT_MANIFEST_PKG_JSON}' '$(strip ${1})'
+	cp -R 'doc\' '$(strip ${1})\'
 	@echo "-> cp source finish"
 
 	tar -zcvf $(strip ${2})${ENV_INFO_DIST_BIN_NAME}-$(strip ${3})-${ENV_INFO_DIST_VERSION}${ENV_INFO_DIST_MARK}.tar.gz -C $(strip ${1}) "."
