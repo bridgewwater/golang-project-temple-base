3ffc1abf65164b4e9424e56f06450082b16ea3fa
sinlov <sinlovgmppt@gmail.com>
2023-02-08 11:53:13 +0800

    ci: update MakeDocker.mk

--- MakefileUtils/MakeDocker.mk
+++ MakefileUtils/MakeDocker.mk
@@ -1,19 +1,19 @@
-#INFO_BUILD_DOCKER_TAG = ${ENV_DIST_VERSION}
-INFO_BUILD_DOCKER_TAG = latest
-INFO_BUILD_DOCKER_FILE = Dockerfile
-INFO_REPOSITORY=${ROOT_NAME}
-INFO_OWNER=bridgewwater
-# if set INFO_PRIVATE_DOCKER_REGISTRY= will push to hub.docker.com
-# private docker registry use harbor must create project name as ${INFO_OWNER}
-#INFO_PRIVATE_DOCKER_REGISTRY=harbor.xxx.com/
-INFO_PRIVATE_DOCKER_REGISTRY=
-INFO_BUILD_DOCKER_SOURCE_IMAGE ?= ${INFO_OWNER}/${INFO_REPOSITORY}
-INFO_BUILD_FROM_IMAGE ?= alpine:3.17
+#ENV_INFO_BUILD_DOCKER_TAG = ${ENV_DIST_VERSION}
+ENV_INFO_BUILD_DOCKER_TAG = latest
+ENV_INFO_BUILD_DOCKER_FILE = Dockerfile
+ENV_INFO_DOCKER_REPOSITORY=${ROOT_NAME}
+ENV_INFO_DOCKER_OWNER=bridgewwater
+# if set ENV_INFO_PRIVATE_DOCKER_REGISTRY= will push to hub.docker.com
+# private docker registry use harbor must create project name as ${ENV_INFO_DOCKER_OWNER}
+#ENV_INFO_PRIVATE_DOCKER_REGISTRY=harbor.xxx.com/
+ENV_INFO_PRIVATE_DOCKER_REGISTRY=
+ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE ?= ${ENV_INFO_DOCKER_OWNER}/${ENV_INFO_DOCKER_REPOSITORY}
+ENV_INFO_BUILD_DOCKER_FROM_IMAGE ?= alpine:3.17
 
-INFO_TEST_BUILD_DOCKER_FILE ?= Dockerfile.s6
-INFO_TEST_BUILD_PARENT_IMAGE ?= golang:1.17.13-buster
-INFO_TEST_BUILD_PARENT_CONTAINER ?= "test-parent-${INFO_REPOSITORY}"
-INFO_TEST_TAG_BUILD_CONTAINER_NAME ?= "test-${INFO_REPOSITORY}"
+ENV_INFO_TEST_BUILD_DOCKER_FILE ?= Dockerfile.s6
+ENV_INFO_TEST_BUILD_DOCKER_PARENT_IMAGE ?= golang:1.17.13-buster
+ENV_INFO_TEST_BUILD_DOCKER_PARENT_CONTAINER ?= "test-parent-${ENV_INFO_DOCKER_REPOSITORY}"
+ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME ?= "test-${ENV_INFO_DOCKER_REPOSITORY}"
 
 ## change this for ip-v4 get
 #ROOT_LOCAL_IP_V4_LINUX = $$(ifconfig enp8s0 | grep inet | grep -v inet6 | cut -d ':' -f2 | cut -d ' ' -f1)
@@ -29,25 +29,25 @@ INFO_TEST_TAG_BUILD_CONTAINER_NAME ?= "test-${INFO_REPOSITORY}"
 
 dockerEnv:
 	@echo "== docker env print start"
-	@echo "INFO_REPOSITORY                     ${INFO_REPOSITORY}"
-	@echo "INFO_OWNER                          ${INFO_OWNER}"
-	@echo "INFO_BUILD_DOCKER_IMAGE_NAME        ${INFO_BUILD_DOCKER_SOURCE_IMAGE}"
-	@echo "INFO_BUILD_DOCKER_TAG               ${INFO_BUILD_DOCKER_TAG}"
-	@echo "INFO_BUILD_DOCKER_FILE              ${INFO_BUILD_DOCKER_FILE}"
-	@echo "INFO_BUILD_FROM_IMAGE               ${INFO_BUILD_FROM_IMAGE}"
-	@echo "INFO_PRIVATE_DOCKER_REGISTRY        ${INFO_PRIVATE_DOCKER_REGISTRY}"
-	@echo "REGISTRY tag as:                    ${INFO_PRIVATE_DOCKER_REGISTRY}${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}"
+	@echo "INFO_REPOSITORY                     ${ENV_INFO_DOCKER_REPOSITORY}"
+	@echo "INFO_OWNER                          ${ENV_INFO_DOCKER_OWNER}"
+	@echo "INFO_BUILD_DOCKER_IMAGE_NAME        ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}"
+	@echo "INFO_BUILD_DOCKER_TAG               ${ENV_INFO_BUILD_DOCKER_TAG}"
+	@echo "INFO_BUILD_DOCKER_FILE              ${ENV_INFO_BUILD_DOCKER_FILE}"
+	@echo "INFO_BUILD_FROM_IMAGE               ${ENV_INFO_BUILD_DOCKER_FROM_IMAGE}"
+	@echo "INFO_PRIVATE_DOCKER_REGISTRY        ${ENV_INFO_PRIVATE_DOCKER_REGISTRY}"
+	@echo "REGISTRY tag as:                    ${ENV_INFO_PRIVATE_DOCKER_REGISTRY}${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}"
 	@echo ""
-	@echo "INFO_TEST_BUILD_PARENT_IMAGE        ${INFO_TEST_BUILD_PARENT_IMAGE}"
-	@echo "INFO_TEST_BUILD_PARENT_CONTAINER    ${INFO_TEST_BUILD_PARENT_CONTAINER}"
+	@echo "INFO_TEST_BUILD_PARENT_IMAGE        ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_IMAGE}"
+	@echo "INFO_TEST_BUILD_PARENT_CONTAINER    ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_CONTAINER}"
 	@echo ""
-	@echo "INFO_TEST_TAG_BUILD_CONTAINER_NAME  ${INFO_TEST_TAG_BUILD_CONTAINER_NAME}"
+	@echo "INFO_TEST_TAG_BUILD_CONTAINER_NAME  ${ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME}"
 	@echo ""
 	@echo "== docker env print end"
 
 dockerAllPull:
-	docker pull ${INFO_BUILD_FROM_IMAGE}
-	docker pull ${INFO_TEST_BUILD_PARENT_IMAGE}
+	docker pull ${ENV_INFO_BUILD_DOCKER_FROM_IMAGE}
+	docker pull ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_IMAGE}
 
 dockerCleanImages:
 	(while :; do echo 'y'; sleep 3; done) | docker image prune
@@ -57,73 +57,73 @@ dockerCleanPruneAll:
 	(while :; do echo 'y'; sleep 3; done) | docker image prune
 
 dockerRunContainerParentBuild:
-	@echo "run rm container image: ${INFO_TEST_BUILD_PARENT_IMAGE}"
-	# docker run -d --rm --name ${INFO_TEST_BUILD_PARENT_CONTAINER} ${INFO_TEST_BUILD_PARENT_IMAGE}
-	docker run -d --rm --name ${INFO_TEST_BUILD_PARENT_CONTAINER} ${INFO_TEST_BUILD_PARENT_IMAGE} tail -f /dev/null
+	@echo "run rm container image: ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_IMAGE}"
+	# docker run -d --rm --name ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_CONTAINER} ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_IMAGE}
+	docker run -d --rm --name ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_CONTAINER} ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_IMAGE} tail -f /dev/null
 	@echo ""
-	@echo "-> run rm container name: ${INFO_TEST_BUILD_PARENT_CONTAINER}"
-	@echo "-> into container use: docker exec -it ${INFO_TEST_BUILD_PARENT_CONTAINER} bash"
+	@echo "-> run rm container name: ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_CONTAINER}"
+	@echo "-> into container use: docker exec -it ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_CONTAINER} bash"
 
 dockerRmContainerParentBuild:
-	-docker rm -f ${INFO_TEST_BUILD_PARENT_CONTAINER}
+	-docker rm -f ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_CONTAINER}
 
 dockerPruneContainerParentBuild: dockerRmContainerParentBuild
-	-docker rmi -f ${INFO_TEST_BUILD_PARENT_IMAGE}
+	-docker rmi -f ${ENV_INFO_TEST_BUILD_DOCKER_PARENT_IMAGE}
 
 dockerTestBuildLatest:
-	docker build --rm=true --tag ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG} --file ${INFO_TEST_BUILD_DOCKER_FILE} .
+	docker build --rm=true --tag ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG} --file ${ENV_INFO_TEST_BUILD_DOCKER_FILE} .
 
 dockerTestRunLatest:
-	docker image inspect --format='{{ .Created}}' ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}
-	-docker run --rm --name ${INFO_TEST_TAG_BUILD_CONTAINER_NAME} \
+	docker image inspect --format='{{ .Created}}' ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}
+	-docker run --rm --name ${ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME} \
 	-e RUN_MODE=dev \
-	${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}
+	${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}
 	# for inner check can use like this
-	# docker run -it -d --entrypoint /bin/sh --name ${INFO_TEST_TAG_BUILD_CONTAINER_NAME} ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}
-	-docker inspect --format='{{ .State.Status}}' ${INFO_TEST_TAG_BUILD_CONTAINER_NAME}
+	# docker run -it -d --entrypoint /bin/sh --name ${ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME} ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}
+	-docker inspect --format='{{ .State.Status}}' ${ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME}
 
 dockerTestLogLatest:
-	-docker logs ${INFO_TEST_TAG_BUILD_CONTAINER_NAME}
+	-docker logs ${ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME}
 
 dockerTestRmLatest:
-	-docker rm -f ${INFO_TEST_TAG_BUILD_CONTAINER_NAME}
+	-docker rm -f ${ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME}
 
 dockerTestRmiLatest:
-	-INFO_TEST_TAG_BUILD_CONTAINER_NAME=$(INFO_TEST_TAG_BUILD_CONTAINER_NAME) \
-	INFO_BUILD_DOCKER_SOURCE_IMAGE=$(INFO_BUILD_DOCKER_SOURCE_IMAGE) \
-	ROOT_DOCKER_IMAGE_TAG=$(INFO_BUILD_DOCKER_TAG) \
-	INFO_BUILD_DOCKER_TAG=${INFO_BUILD_DOCKER_TAG} \
-	docker rmi -f ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}
+	-INFO_TEST_TAG_BUILD_CONTAINER_NAME=$(ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME) \
+	INFO_BUILD_DOCKER_SOURCE_IMAGE=$(ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE) \
+	ROOT_DOCKER_IMAGE_TAG=$(ENV_INFO_BUILD_DOCKER_TAG) \
+	INFO_BUILD_DOCKER_TAG=${ENV_INFO_BUILD_DOCKER_TAG} \
+	docker rmi -f ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}
 
 dockerTestRestartLatest: dockerTestRmLatest dockerTestRmiLatest dockerTestBuildLatest dockerTestRunLatest
-	@echo "restart ${INFO_TEST_TAG_BUILD_CONTAINER_NAME} ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}"
+	@echo "restart ${ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME} ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}"
 
 dockerTestStopLatest: dockerTestRmLatest dockerTestRmiLatest
-	@echo "stop and remove ${INFO_TEST_TAG_BUILD_CONTAINER_NAME} ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}"
+	@echo "stop and remove ${ENV_INFO_TEST_TAG_BUILD_DOCKER_CONTAINER_NAME} ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}"
 
 dockerTestPruneLatest: dockerTestStopLatest
-	@echo "prune and remove ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}"
+	@echo "prune and remove ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}"
 
 dockerRmiBuild:
-	-docker rmi -f ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}
-	-docker rmi -f ${INFO_PRIVATE_DOCKER_REGISTRY}${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}
+	-docker rmi -f ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}
+	-docker rmi -f ${ENV_INFO_PRIVATE_DOCKER_REGISTRY}${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}
 
 dockerBuild:
-	docker build --rm=true --tag ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG} --file ${INFO_BUILD_DOCKER_FILE} .
+	docker build --rm=true --tag ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG} --file ${ENV_INFO_BUILD_DOCKER_FILE} .
 
 dockerTag:
-	docker tag ${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG} ${INFO_PRIVATE_DOCKER_REGISTRY}${INFO_BUILD_DOCKER_SOURCE_IMAGE}
+	docker tag ${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG} ${ENV_INFO_PRIVATE_DOCKER_REGISTRY}${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}
 
 dockerBeforePush: dockerRmiBuild dockerBuild dockerTag
-	@echo "===== then now can push to ${INFO_PRIVATE_DOCKER_REGISTRY}"
+	@echo "===== then now can push to ${ENV_INFO_PRIVATE_DOCKER_REGISTRY}"
 
 dockerPushBuild: dockerBeforePush
-	docker push ${INFO_PRIVATE_DOCKER_REGISTRY}${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}
-	@echo "=> push ${INFO_PRIVATE_DOCKER_REGISTRY}${INFO_BUILD_DOCKER_SOURCE_IMAGE}:${INFO_BUILD_DOCKER_TAG}"
+	docker push ${ENV_INFO_PRIVATE_DOCKER_REGISTRY}${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}
+	@echo "=> push ${ENV_INFO_PRIVATE_DOCKER_REGISTRY}${ENV_INFO_BUILD_DOCKER_SOURCE_IMAGE}:${ENV_INFO_BUILD_DOCKER_TAG}"
 
 helpDocker:
 	@echo "=== this make file can include MakeDocker.mk then use"
-	@echo "- must has file: [ ${INFO_BUILD_DOCKER_FILE} ${INFO_TEST_BUILD_DOCKER_FILE}" ]
+	@echo "- must has file: [ ${ENV_INFO_BUILD_DOCKER_FILE} ${ENV_INFO_TEST_BUILD_DOCKER_FILE}" ]
 	@echo "- then change tag as:                       INFO_BUILD_DOCKER_TAG"
 	@echo "- then change repository as:                INFO_REPOSITORY"
 	@echo "- then change owner as:                     INFO_OWNER"
@@ -141,12 +141,12 @@ helpDocker:
 	@echo "- and prune resource at parent image"
 	@echo "$$ make dockerPruneContainerParentBuild"
 	@echo ""
-	@echo "- test run container use ./${INFO_TEST_BUILD_DOCKER_FILE}"
+	@echo "- test run container use ./${ENV_INFO_TEST_BUILD_DOCKER_FILE}"
 	@echo "$$ make dockerTestRestartLatest"
 	@echo "- prune test container and image"
 	@echo "$$ make dockerTestPruneLatest"
 	@echo ""
-	@echo "- build and tag as use ./${INFO_BUILD_DOCKER_FILE}"
+	@echo "- build and tag as use ./${ENV_INFO_BUILD_DOCKER_FILE}"
 	@echo "$$ make dockerBeforePush"
 	@echo "- prune build"
 	@echo "$$ make dockerRmiBuild"
