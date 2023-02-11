ad89634e20701da5666350a8348771ad1643e708
sinlov <sinlovgmppt@gmail.com>
2023-02-11 17:05:14 +0800

    feat: update drone.yml

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"os"
 	"os/exec"
 	"testing"
@@ -10,10 +11,15 @@ func Test_package_main(t *testing.T) {
 
 	cmd := exec.Command(os.Args[0], "-h")
 	cmd.Env = append(os.Environ(), "ENV_WEB_AUTO_HOST=true")
+	var outStd bytes.Buffer
+	cmd.Stdout = &outStd
+	var errStd bytes.Buffer
+	cmd.Stderr = &errStd
 	err := cmd.Run()
 	if err != nil {
 		t.Fatal(err)
 	}
+	t.Logf("-h result: %s\n%s", outStd.String(), errStd.String())
 
 	cmdFail := exec.Command(os.Args[0], "--error.arg")
 	cmdFail.Env = append(os.Environ(), "ENV_WEB_AUTO_HOST=true")
